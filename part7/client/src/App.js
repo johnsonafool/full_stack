7ea8e302194a2b73700287ea21a77091f74ec655
johnsonafool <88415078+johnsonafool@users.noreply.github.com>
2022-08-31 18:20:45 +0800

    add: use route to handle diff pages

--- part7/client/src/App.js
+++ part7/client/src/App.js
@@ -1,15 +1,16 @@
-import { useState } from "react";
 import ReactDOM from "react-dom/client";
+
+import {
+  BrowerRouter as Router,
+  Link,
+  Navigate,
+  Route,
+  Routes,
+} from "react-router";
 
 const Home = () => (
   <div>
     <h2>TKTL notes app</h2>
-  </div>
-);
-
-const Notes = () => (
-  <div>
-    <h2>Notes</h2>
   </div>
 );
 
@@ -20,42 +21,54 @@ const Users = () => (
 );
 
 const App = () => {
-  const [page, setPage] = useState("home");
-
-  const toPage = (page) => (e) => {
-    e.preventDefault();
-    setPage(page);
-  };
-
-  const content = () => {
-    if (page === "home") {
-      return <Home />;
-    } else if (page === "notes") {
-      return <Notes />;
-    } else if (page === "users") {
-      return <Users />;
-    }
-  };
-
+  const Notes = ({ notes }) => (
+    <div>
+      <h2>Notes</h2>
+      <ul>
+        {notes.map((note) => (
+          <li key={note.id}>
+            <Link to={`/notes/${note.id}`}>{note.content}</Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
   const padding = {
     padding: 5,
   };
 
   return (
-    <div>
+    // Inside the router, we define links that modify
+    // the address bar with the help of the Link component
+    <Router>
       <div>
-        <a href="" onClick={toPage("home")} style={padding}>
-          home
-        </a>
-        <a href="" onClick={toPage("notes")} style={padding}>
-          notes
-        </a>
-        <a href="" onClick={toPage("users")} style={padding}>
-          users
-        </a>
+        <div>
+          <Link style={padding} to="/">
+            home
+          </Link>
+          <Link style={padding} to="/notes">
+            notes
+          </Link>
+          <Link style={padding} to="/users">
+            users
+          </Link>
+        </div>
+        <Routes>
+          {/* Components rendered based on the URL of the browser are defined with the help of the component Route */}
+          <Route path="/notes/:id" element={<Note notes={notes} />} />
+          <Route path="/notes" element={<Notes notes={notes} />} />
+          <Route
+            path="/users"
+            element={user ? <Users /> : <Navigate replace to="/login" />}
+          />
+          <Route path="/login" element={<Login onLogin={login} />} />
+          <Route path="/" element={<Home />} />
+        </Routes>
+        <div>
+          <i>Note app</i>
+        </div>
       </div>
-      {content()}
-    </div>
+    </Router>
   );
 };
 
