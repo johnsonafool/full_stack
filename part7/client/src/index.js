08a4bdba8a1f016d0497402177f32ef83401a4d8
johnsonafool <88415078+johnsonafool@users.noreply.github.com>
2022-08-31 18:35:00 +0800

    add: part7 ex part

--- part7/client/src/index.js
+++ part7/client/src/index.js
@@ -1,16 +1,10 @@
 import ReactDOM from "react-dom/client";
-import { Provider } from "react-redux";
 import App from "./App";
-import store from "./store";
 
-// const store = configureStore({
-//   reducer: { notes: noteReucer, filter: filterReducer },
-// });
-
-// noteService.getAll().then((notes) => store.dispatch(setNotes(notes)));
+import { BrowerRouter as Router } from "react-router";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <Provider store={store}>
+  <Router>
     <App />
-  </Provider>
+  </Router>
 );
