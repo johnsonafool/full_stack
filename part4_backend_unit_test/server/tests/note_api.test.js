2602df219b1a7cbe0c74cc546863fe90716998a9
johnsonafool <88415078+johnsonafool@users.noreply.github.com>
2022-09-03 21:10:04 +0800

    add: part7-webpack branch

---
+++ part4_backend_unit_test/server/tests/note_api.test.js
@@ -0,0 +1,17 @@
+// supertest for sake of APi testing
+const mongoose = require("mongoose");
+const supertest = require("supertest");
+const app = require("../app");
+
+const api = supertest(app);
+
+test("notes are returned as json", async () => {
+  await api
+    .get("/api/notes")
+    .expect(200)
+    .expect("Content-Type", /application\/json/);
+});
+
+afterAll(() => {
+  mongoose.connection.close();
+});
